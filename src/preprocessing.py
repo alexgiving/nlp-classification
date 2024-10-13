40a4e44b812aceeffefc477fbd57545078177885
Olga <olga.frolova.2805@gmail.com>
2024-10-13 22:00:04 +0300

    add new function for EDA

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,21 @@
 from pathlib import Path
-
+import numpy as np
 import pandas as pd
+import random
+import re
 
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+def set_seed(seed_value=123):
+    np.random.seed(seed_value)
+    random.seed(seed_value)
 
 def load_dataset(dataset_path: Path) -> pd.DataFrame:
     dataset_header = ['id', 'entity', 'sentiment', 'content']
@@ -12,3 +26,45 @@ def load_dataset(dataset_path: Path) -> pd.DataFrame:
         names=dataset_header
     )
     return dataset
+
+def remove_stop_words(text: str) -> str:
+    words = word_tokenize(text)
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+    return ' '.join(filtered_words)
+
+def remove_special_characters(text: str) -> str:
+    clean_text = re.sub(r'[^A-Za-z0-9\s]', '', text)
+    return clean_text
+
+def convert_to_lowercase(text: str) -> str:
+    return text.lower()
+
+def remove_extra_spaces(text: str) -> str:    
+    return re.sub(r'\s+', ' ', text.strip())
+
+def lemmatization(text: str) -> str:
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    result_text = []
+    for word in text:
+        result_text.append(lemmatizer.lemmatize(word))
+    return ' '.join(result_text)
+
+def stemming(text: str) -> str:
+    stemmer = PorterStemmer()
+    text = text.split()
+    result_text = []
+    for word in text:
+        result_text.append(stemmer.stem(word))
+    return ' '.join(result_text)
+
+def normalization_text(type_normalization: str, text: str) -> str:
+    if type_normalization == 'stemming':
+        return stemming(text)
+    elif type_normalization == 'lemmatization':
+        return lemmatization(text)
+    elif type_normalization == 'combo':
+        text = lemmatization(text)
+        return stemming(text)
+    else:
+        raise ValueError(f"Unsupported normalization type: {type_normalization}. Avaliable options: ['stemming', 'lemmatization', 'combo'(lemmatization + stemming)]")
--- src/visualization.py
+++ src/visualization.py
@@ -1,18 +1,62 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+from wordcloud import WordCloud
 
-
-def distribution_chart(data: pd.Series) -> plt.Figure:
+def distribution_chart(data: pd.Series, column_name: str) -> plt.Figure:
     fig, ax = plt.subplots()
 
     bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
 
-    ax.bar(
+    ax.barh(
         [name for name, _ in data.items()],
         data.values,
         color = bar_colors
         )
 
-    ax.set_ylabel('Number of samples')
-    ax.set_title('Distribution of samples')
+    ax.set_ylabel(f'Number of {column_name}')
+    ax.set_title(f'Distribution of {column_name}')
+    return fig
+
+def sentiment_distribution_chart(data: pd.DataFrame, entity_col: str, sentiment_col: str) -> plt.Figure:
+    fig, ax = plt.subplots(figsize=(18, 15))
+
+    sns.countplot(
+        data=data,
+        y=entity_col,
+        hue=sentiment_col,
+        order=data[entity_col].value_counts().index,
+        palette='viridis',
+        ax=ax
+    )
+
+    ax.set_title('Sentiment Distribution by Entity')
+    ax.set_xlabel('Count')
+    ax.set_ylabel('Entity')
+    ax.legend(title='Sentiment')
+    
+    plt.tight_layout()
+
+    return fig
+
+def generate_sentiment_wordclouds(data: pd.DataFrame,
+                                  sentiment_col: str,
+                                  text_col: str,
+                                  sentiments:list,
+                                  background_color: str = 'white',
+                                  width: int = 400,
+                                  height: int = 200
+) -> plt.Figure:
+   
+    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+
+    for sentiment, ax in zip(sentiments, axs.ravel()):
+        sentiment_text = " ".join(tweet for tweet in data[data[sentiment_col] == sentiment][text_col])
+        wordcloud = WordCloud(background_color=background_color, width=width, height=height).generate(sentiment_text)
+        
+        ax.imshow(wordcloud, interpolation='bilinear')
+        ax.axis('off')
+        ax.set_title(f'Word Cloud for {sentiment} Sentiment')
+    
+    plt.tight_layout()
     return fig
