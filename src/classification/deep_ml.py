a919c7a1a0500b40791298086f1f0b8f53bb3994
Trutnev Aleksei <alexgiving@mail.ru>
2024-10-24 12:08:13 +0200

    init dl

--- src/classification/deep_ml.py
+++ src/classification/deep_ml.py
@@ -1,134 +1,58 @@
-from typing import List, Tuple
+from abc import ABC, abstractmethod
 
-import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
-from tqdm.auto import tqdm
+from datasets import Dataset
+from transformers import AutoTokenizer
+from transformers.modeling_outputs import SequenceClassifierOutput
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-class CustomModel(nn.Module):
-    def __init__(self,
-                 hidden_dim: int,
-                 vocab_size: int,
-                 num_classes: int,
-                 num_rnn_layers: int,
-                 p_drop_out: float=0.1) -> None:
+def prepare_dataset(x: pd.Series, y: pd.Series, tokenizer: AutoTokenizer) -> Dataset:
+    tokenized_inputs = tokenizer(list(x), padding='max_length', truncation=True, return_tensors='pt')
+    dataset = Dataset.from_dict(
+        {
+            'input_ids': tokenized_inputs['input_ids'],
+            'label': torch.tensor(y, dtype=torch.long)
+        }
+    )
+    return dataset
+
+
+class BaseClassifier(ABC, nn.Module):
+    def __init__(self, vocab_size, embedding_dim=128, hidden_dim=256, num_layers=1, num_classes=2, dropout=0.5):
         super().__init__()
-        self._hidden_dim = hidden_dim
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.backbone = self._get_backbone(embedding_dim=embedding_dim, hidden_dim=hidden_dim, num_layers=num_layers)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_dim, num_classes)
 
-        self._embedding = nn.Embedding(vocab_size, hidden_dim)
-        self._linear = nn.Linear(hidden_dim, hidden_dim)
-        self._classifier = nn.Linear(hidden_dim, num_classes)
-        self._activation = nn.Tanh()
-        self._dropout = nn.Dropout(p=p_drop_out)
+    def forward(self, input_ids=None, labels=None, **kwargs):
+        embedded = self.embedding(input_ids)
+        lstm_out, _ = self.backbone(embedded)
+        last_hidden_state = lstm_out[:, -1, :]
+        logits = self.fc(self.dropout(last_hidden_state))
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        output = self._embedding(x)
-        output, _ = self._rnn(output)
-        output = output.mean(dim=1)
-        output = self._activation(output)
-        output = self._linear(output)
-        output = self._dropout(output)
-        output = self._activation(output)
-        return self._classifier(output)
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits, labels)
 
+        return SequenceClassifierOutput(loss=loss, logits=logits)
 
-class RNNModel(CustomModel):
-    def __init__(self,
-                 hidden_dim: int,
-                 vocab_size: int,
-                 num_classes: int,
-                 num_rnn_layers: int,
-                 p_drop_out: float=0.1) -> None:
-        super().__init__(
-            hidden_dim,
-            vocab_size,
-            num_classes,
-            num_rnn_layers,
-            p_drop_out
-        )
-        self._rnn = nn.RNN(
-            self._hidden_dim,
-            self._hidden_dim,
-            num_layers=num_rnn_layers
-        )
+    @abstractmethod
+    def _get_backbone(self, *args, **kwargs) -> nn.Module:
+        pass
+
+    def get_name(self) -> str:
+        return self.__class__.__name__
 
 
-class LSTMModel(CustomModel):
-    def __init__(self,
-                 hidden_dim: int,
-                 vocab_size: int,
-                 num_classes: int,
-                 num_rnn_layers: int,
-                 p_drop_out: float=0.1) -> None:
-        super().__init__(
-            hidden_dim,
-            vocab_size,
-            num_classes,
-            num_rnn_layers,
-            p_drop_out
-        )
-        self._rnn = nn.LSTM(
-            self._hidden_dim,
-            self._hidden_dim,
-            num_layers=num_rnn_layers
-        )
+class LSTMClassifier(BaseClassifier):
+    def _get_backbone(self, *args, **kwargs) -> nn.Module:
+        return nn.LSTM(kwargs['embedding_dim'], kwargs['hidden_dim'], num_layers=kwargs['num_layers'], batch_first=True)
 
-def train_epoch(
-        model: nn.Module,
-        data_loader: DataLoader,
-        optimizer: torch.optim.Optimizer,
-        criterion: nn.Module
-    ) -> float:
-    losses = []
 
-    for train_batch in tqdm(data_loader):
-        optimizer.zero_grad()
-
-        res = model(train_batch['input_ids'].to(device))
-        loss = criterion(res, train_batch['label'])
-        loss.backward()
-        optimizer.step()
-
-        losses.append(loss.cpu().detach().item())
-    return np.mean(losses)
-
-def evaluate(model, eval_dataloader) -> float:
-    predictions = []
-    target = []
-    with torch.no_grad():
-        for batch in eval_dataloader:
-            logits = model(batch['input_ids'])
-            predictions.append(logits.argmax(dim=1))
-            target.append(batch['label'])
-
-    predictions = torch.cat(predictions)
-    target = torch.cat(target)
-    accuracy = (predictions == target).float().mean().item()
-
-    return accuracy
-
-def train(
-        model: nn.Module,
-        criterion: nn.Module,
-        optimizer: torch.optim.Optimizer,
-        num_epochs: int,
-        train_loader: DataLoader,
-        val_loader: DataLoader
-    ) -> Tuple[List[float], List[float]]:
-    train_losses = []
-    accuracies = []
-
-    for epoch_id in range(num_epochs):
-        model.train()
-        train_loss = train_epoch(model, train_loader, optimizer, criterion)
-        train_losses.append(train_loss)
-
-        model.eval()
-        with torch.inference_mode():
-            accuracy = evaluate(model, val_loader)
-        accuracies.append(accuracy)
-    return train_losses, accuracies
-
+class RNNClassifier(BaseClassifier):
+    def _get_backbone(self, *args, **kwargs) -> nn.Module:
+        return nn.RNN(kwargs['embedding_dim'], kwargs['hidden_dim'], num_layers=kwargs['num_layers'], batch_first=True)
